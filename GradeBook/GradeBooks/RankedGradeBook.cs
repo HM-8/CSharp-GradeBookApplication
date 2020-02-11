5ba6d9eb7fa61e9781eb7f278738b909ad31ffd0
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-02-11 23:12:07 +0300

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,25 +16,20 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            }
-            else
-            { 
-                base.CalculateStatistics();
-            }
+                return;
+            } 
+            base.CalculateStatistics();
         }
 
-        public override string CalculateStudentStatistics()
+        public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
-            else
-            { 
-                base.CalculateStudentStatistics(); 
-            }
+            base.CalculateStudentStatistics(name);
         }
-
 
         public override char GetLetterGrade(double averageGrade)
         {
