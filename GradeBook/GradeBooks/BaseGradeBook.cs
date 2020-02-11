c10f33b0f9071d72bbc89769780b668bff9ea929
Hiwot Medhin <hiwotmedhings@gmail.com>
2020-02-11 13:38:21 +0300

    Updated the BaseGradeBook class
    Added a public variable "Type" with the type of GradebookType

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,10 +9,13 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
+
     public class BaseGradeBook
     {
+        public GradeBookType Type;
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+
 
         public BaseGradeBook(string name)
         {
